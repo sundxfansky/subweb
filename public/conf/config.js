a2453cce7ff9adf07849b795b165f2081ddf262f
sundxfansky <sundxai@gmail.com>
2025-07-08 11:41:47 +0800

    Update config.js

--- public/conf/config.js
+++ public/conf/config.js
@@ -2,20 +2,15 @@ window.config = {
   // 网站标题
   siteName: 'Subconverter Web',
   // API 地址
-  apiUrl: 'http://127.0.0.1:25500',
+  apiUrl: 'https://bksub.sg2.sdxx.de/',
   // 短域名服务地址
-  shortUrl: 'https://s.ops.ci',
+  shortUrl: 'https://url.sg2.sdxx.de',
   // 首页菜单
   menuItem: [
     {
       title: '首页',
       link: '/',
       target: '',
-    },
-    {
-      title: 'GitHub',
-      link: 'https://github.com/stilleshan/subweb',
-      target: '_blank',
     },
   ],
   // 远程配置地址,可以自行按照格式添加。
